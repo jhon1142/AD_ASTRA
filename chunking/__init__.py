71f0a4a422127b54f3446775f64352d460e822c6
jhon1142 <jhon971014@gmail.com>
2026-08-12 22:11:50 -0500

    debug

--- chunking/__init__.py
+++ chunking/__init__.py
@@ -1,9 +1,7 @@
-from chunking.splitter import RecursiveCharacterSplitter
 from chunking.sentence_splitter import SentenceSplitter
 from chunking.token_counter import TokenCounter
 
 __all__ = [
-    "RecursiveCharacterSplitter",
     "SentenceSplitter",
     "TokenCounter",
 ]
